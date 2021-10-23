0d2514a62989fa1610d1b5e9bdc0dd962e2e98cf
mohmedeprahem <m.eprahem.168@gmai.com>
2021-10-23 16:55:57 +0200

    same proble has solved before

--- ReverseWordsinaString.ts
+++ ReverseWordsinaString.ts
@@ -1,3 +1,4 @@
+export {};
 function reverseWords(s: string): string {
   let word = '',
   isSwapped = false,
@@ -5,21 +6,19 @@ function reverseWords(s: string): string {
 
   // jumb to each index in string
   for (let i = 0; i < s.length ; i++) {
-  // if it char so save it.
-  while (s[i]) {
-    isSwapped = true;
-    
-    word.concat(s[i]);
-    
-    i++;
-  };
+    // if it char so save it.
+    while (s[i] != ' ' && i < s.length) {
+      isSwapped = true;
+      word += s[i];
+      i++;
+    };
 
-  // reverse word
-  if (isSwapped)
-    newText = word + ' ' + newText;
-  };
-
-  isSwapped = false;
-
-  return newText;
-};
+    // reverse word
+    if (isSwapped) {
+      newText = word + ' ' + newText;
+      word = '';
+      isSwapped = false;
+    }
+  }
+  return newText.substring(0, newText.length - 1);
+}
